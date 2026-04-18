ba790ef4862ba7b3dd67336a7a4e74332b461a17
catyugu <141992786+catyugu@users.noreply.github.com>
2026-04-19 01:27:36 +0800

    feat: add transient simulation and thermal mapping scripts to run.sh

--- examples/example5/run.sh
+++ examples/example5/run.sh
@@ -14,3 +14,11 @@ cp outputs/example.steady example.init
 
 # Transient simulation
 ../../hotspot -c example.config -p example.ptrace -materials_file example.materials -grid_layer_file example.lcf -init_file example.init -model_type grid -detailed_3D on -use_microchannels 1 -o outputs/example.transient -grid_transient_file outputs/example.grid.ttrace
+
+python ../../scripts/split_grid_steady.py outputs/example.grid.steady 6 64 64
+python ../../scripts/grid_thermal_map.py ev6_3D_cache_1.flp outputs/example_layer0.grid.steady 64 64 outputs/layer0.png
+python ../../scripts/grid_thermal_map.py ev6_3D_TIM_TSV.flp outputs/example_layer1.grid.steady 64 64 outputs/layer1.png
+python ../../scripts/grid_thermal_map.py ev6_3D_cache_2.flp outputs/example_layer2.grid.steady 64 64 outputs/layer2.png
+python ../../scripts/grid_thermal_map.py ev6_3D_TIM_TSV.flp outputs/example_layer3.grid.steady 64 64 outputs/layer3.png
+python ../../scripts/grid_thermal_map.py ev6_3D_core_layer.flp outputs/example_layer4.grid.steady 64 64 outputs/layer4.png
+python ../../scripts/grid_thermal_map.py ev6_3D_TIM.flp outputs/example_layer5.grid.steady 64 64 outputs/layer5.png
